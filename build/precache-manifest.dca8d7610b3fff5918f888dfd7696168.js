aab6cce05d2b8a6eaaf3f3afffc51647e8e3665e
Gabin-ishimwe <g.ishimwe@alustudent.com>
2022-04-30 00:03:24 +0200

    deploy build

---
+++ build/precache-manifest.dca8d7610b3fff5918f888dfd7696168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1ba5b8636e15c69cf05ea548706e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd5bc4e6ad281337699",
+    "url": "/static/css/2.ef07fde4.chunk.css"
+  },
+  {
+    "revision": "d789f6a7e85573ef3655",
+    "url": "/static/css/main.5151746d.chunk.css"
+  },
+  {
+    "revision": "8bd5bc4e6ad281337699",
+    "url": "/static/js/2.a5dadc70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5dadc70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d789f6a7e85573ef3655",
+    "url": "/static/js/main.51485899.chunk.js"
+  },
+  {
+    "revision": "d95c3e794f2c446ee055",
+    "url": "/static/js/runtime-main.9d9188f7.js"
+  }
+]);
